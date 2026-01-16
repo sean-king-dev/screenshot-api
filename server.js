a9dbfba144eab0877fb86e31f75aad06aabbd796
Sean King <sean.king@kingseducation.com>
2026-01-16 11:45:09 +0000

    update bg col css and page height

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+ 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
